f84162d8bb499ed3cba0673d7ec2c65a18902e27
mateus.castro <castro@oceanainvestimentos.com.br>
2022-03-03 15:24:36 -0300

    getStaticProps para gerar páginas em tempo de build getServerSideProps para gerar páginas a cada acesso. Tag Head do Next para editar os metadados

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,33 @@
 import Link from "../src/components/Link";
+import PageTitle from "../src/components/PageTitle";
 
-export default function FaqPage() {
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    const faq = await fetch(FAQ_API_URL)
+        .then((res) => res.json())
+    return {
+        props: {
+            faq
+        }
+    }
+}
+
+export default function FaqPage({faq}) {
     return (
         <div>
+            <PageTitle>Home - Alura Cases FAQ</PageTitle>
             <h1>Alura Cases - FAQ</h1>
             <Link href='/'>Ir para a home</Link>
+            <ul>
+                {faq.map(({answer, question}) => {
+                    return(
+                        <li key={question}>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
