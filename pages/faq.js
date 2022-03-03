81a952afc79b7ae99fedae4d2695f2831620bac8
mateus.castro <castro@oceanainvestimentos.com.br>
2022-03-03 13:47:36 -0300

    Criando um projeto next do zero. Estrutura de pages, component Link para não gerar reload em SPAs

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function FaqPage() {
+    return (
+        <div>
+            <h1>Alura Cases - FAQ</h1>
+            <Link href='/'>Ir para a home</Link>
+        </div>
+    )
+}
